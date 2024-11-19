6bc789eaefe6264ce4aa9892b05deeb2b9ec7e8d
24LP5569 24LP5569 <24LP5569@G304-CC0216.local>
2024-11-19 12:41:15 +0800

    first commit

---
+++ condition.js
@@ -0,0 +1,61 @@
+// let x = 794;
+// let y = 7;
+// let answer = 794 % 7;
+// console.log(answer);
+
+// let too = 70;
+
+// if (too % 7 == 0) {
+//   if (too % 11 == 0) {
+//     console.log("2uuland huvaagdaj baina");
+//   } else {
+//     console.log("11d huvaagdahgui, 7d huvaagdana");
+//   }
+// } else {
+//   if (too % 11 == 0) {
+//     console.log("11d huvaagdana, 7d huvaagdahgui");
+//   } else {
+//     console.log("2uuland huvaagdahgui");
+//   }
+// }
+
+// let too = 3;
+// let answer = 3 % 2;
+// console.log(answer);
+
+// homework
+// 1. Write a program that calculates the total cost of 3 apples and 2 bananas.
+// Each apple costs $1.5, and each banana costs $0.8.
+// Calculate the total cost and print it.
+
+let apples = 1.5;
+let banana = 0.8;
+
+let answer = 1.5 * 3 + 0.8 * 2;
+console.log(answer);
+
+// 2. Write a program that checks if a number is even or odd.
+// Input: A number x.
+// Output: Print whether the number is even or odd.
+
+// 2.5. Write a program that checks if a number is even or odd and positive or negative
+// Input: A number x.
+// Output: should be one of the 4 different text example: "x is even and negative"
+let too = -76;
+if (too % 2 == 0) {
+  if (too < 0) {
+    console.log("tegsh too  surug ");
+  } else {
+    console.log("tegsh too eyreg");
+  }
+} else {
+  if (too < 0) {
+    console.log("sondgoi too surug ");
+  } else {
+    console.log("sondgoi too eyreg");
+  }
+}
+// 3. Create a basic login system.
+// Input: Two variables username and password.
+// If the username is "admin" and the password is "12345", print "Login successful!".
+// Otherwise, print "Login failed!”.
---
+++ imac.js
@@ -0,0 +1,74 @@
+// let x = 10;
+// let y = 20;
+
+// x = y;
+// let niilber = x + y;
+// console.log(niilber);
+
+// let birthyear = 1996;
+// let nowyear = 2024;
+// let answer = birthyear - nowyear;
+// console.log(answer);
+
+// let title = "Baby shark";
+// let line1 = "Baby shark, doo, doo, doo, doo, doo, doo";
+// console.log(line1);
+// console.log(line1);
+// console.log(line1);
+// console.log(title);
+
+// let line3 = "Mommy shark, doo, doo, doo, doo, doo, doo";
+// let title2 = "Mommy shark";
+// console.log(line3);
+// console.log(line3);
+// console.log(line3);
+// console.log(title2);
+
+// let line4 = "Daddy shark, doo, doo, doo, doo, doo, doo";
+// let title3 = "Daddy shark";
+// console.log(line4);
+// console.log(line4);
+// console.log(line4);
+// console.log(title3);
+
+// let line5 = "Grandma shark, doo, doo, doo, doo, doo, doo";
+// let title4 = "Grandma shark";
+// console.log(line5);
+// console.log(line5);
+// console.log(line5);
+// console.log(title4);
+
+// let line6 = "Grandpa shark, doo, doo, doo, doo, doo, doo";
+// let title5 = "Grandpa shark";
+// console.log(line6);
+// console.log(line6);
+// console.log(line6);
+// console.log(title5);
+
+// let line7 = "Let's go hunt, doo, doo, doo, doo, doo, doo";
+// let title6 = "Let's go hunt!";
+// console.log(line7);
+// console.log(line7);
+// console.log(line7);
+// console.log(title6);
+
+// let line8 = "Run away, doo, doo, doo, doo, doo, doo";
+// let title7 = "Run away!";
+// console.log(line8);
+// console.log(line8);
+// console.log(line8);
+// console.log(title7);
+
+// let line9 = "Safe at last, doo, doo, doo, doo, doo, doo";
+// let title8 = "Safe at last!!";
+// console.log(line9);
+// console.log(line9);
+// console.log(line9);
+// console.log(title8);
+
+// let line10 = "It's the end, doo, doo, doo, doo, doo, doo";
+// let title9 = "It's the end!";
+// console.log(line10);
+// console.log(line10);
+// console.log(line10);
+// console.log(title9);
