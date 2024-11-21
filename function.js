37746a8692a3a01dcc956dc1d35c5b928547dd99
24LP5569 24LP5569 <24LP5569@G304-CC0216.local>
2024-11-21 12:30:36 +0800

    function

---
+++ function.js
@@ -0,0 +1,62 @@
+// function nemeh(num1, num2) {
+//   console.log(num1 + num2);
+//   return num1 + num2;
+// }
+// nemeh(4, 8);
+// function hasah(too1, too2) {
+//   console.log(too1 - too2);
+//   return too1 - too2;
+// }
+// hasah(4, 2);
+// function urjih(num1, num2) {
+//   console.log(num1 * num2);
+//   return num1 * num2;
+// }
+// urjih(2, 2);
+// function huvaah(num1, num2) {
+//   console.log(num1 / num2);
+//   return num1 / num2;
+// }
+// huvaah(4 / 2);
+// function uldegdel(num1, num2) {
+//   console.log(num1 % num2);
+//   return num1 % num2;
+// }
+// uldegdel(3, 2);
+
+// n hvrtelh natural toog bichih
+function printnumbers(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+printnumbers(38);
+
+// n hvrtelh toog urvuugar gargah
+function printnumber(n) {
+  for (let i = n; i > 0; i--) {
+    console.log(i);
+  }
+}
+printnumber(100);
+
+// 1ees 100 hvrtelh tegsh toog hevleh
+function printnumber(n) {
+  for (let i = 2; i <= n; i = i + 2) {
+    console.log(i);
+  }
+}
+printnumber(100);
+// 1ees 100 hvrtel sondgoi toog hevleh
+function printnumber(n) {
+  for (let i = 1; i <= n; i = i + 2) {
+    console.log(i);
+  }
+}
+printnumber(100);
+
+// 1ees n hvrtelh natural toonii niilber
+let sum = 0;
+function printnumber(n){
+    for(i=1)
+}
--- loop.js
+++ loop.js
@@ -39,3 +39,40 @@ for (let i = 1; i <= num; i = i + 1) {
   sum = sum + i;
 }
 console.log("sum", sum);
+// Write a C program to find sum of all even numbers between 1 to n.
+let num = 10;
+let sum = 0;
+
+for (let i = 1; i <= num; i = i + 1) {
+  if (i % 2 == 0) {
+    sum = sum + i;
+  }
+}
+console.log(sum);
+// Write a C program to find sum of all odd numbers between 1 to n.
+let num = 10;
+let sum = 0;
+for (let i = 1; i < num; i = i + 2) {
+  sum = sum + i;
+  console.log(sum);
+}
+// Write a C program to print multiplication table of any number.
+let a = 5;
+let sum = 3;
+for (let i = 1; i <= a; i = i + 1) {
+  sum = sum * i;
+  console.log(sum);
+}
+// Write a C program to find all factors of a number.
+let num=3;
+for()
+  // baasan garig baina
+let udur=5;
+  if(udur==1)console.log("monday");
+  if(udur==2)console.log('tuesday');
+  if(udur==3)console.log("wednsday");
+  if(udur==4)console.log("thursday");
+  if(udur==5)console.log("friday");
+  if(udur==6)console.log("saturday");
+  if(udur==7)console.log("sunday");
+  console.log();
