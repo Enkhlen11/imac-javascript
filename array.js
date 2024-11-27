68ce670b353053892b7843daf79406f52f06cc8c
24LP5569 24LP5569 <24LP5569@G304-CC0216.local>
2024-11-27 12:41:36 +0800

    array

---
+++ array.js
@@ -0,0 +1,48 @@
+// ARRAY;
+let ages = [10, 20, 30, 40, 50, 10];
+let names = ["boldo", "dorjo", "dondog", "bataa", "boogii", "haagii"];
+
+console.log(names[4]);
+console.log(ages[4]);
+console.log(ages.length);
+
+// nasnuudiin niilberiig oloy
+function findSumOfAges(ages) {
+  let sum = 0;
+  for (let i = 0; i < ages.length; i++) {
+    sum = sum + ages[i];
+  }
+  return sum;
+}
+
+let answer = findSumOfAges(ages);
+
+let average = answer / ages.length;
+console.log(average);
+
+// \\\
+let num = [48, 10, 10, 30, 40];
+function aver(num) {
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum = sum + num[i];
+  }
+  let answer = sum / num.length;
+  return answer;
+}
+
+console.log(aver(num));
+// array dotorh hamgiin baga utgiig ol
+// array dotorh hamgiin baga ghdee sondgoi utgiig ol
+// array dotorh hamgiin ih tegsh toog ol
+let numbers = [30, 49, 30, 20, 456, 98];
+function findMin(arr) {
+  for (i = 0; i < arr.length; i++) {
+    if (minNam > arr[i + 1]) {
+      maxNum = arr[i + 1];
+    }
+
+    console.log(maxNum);
+  }
+}
+findMin(numbers);
